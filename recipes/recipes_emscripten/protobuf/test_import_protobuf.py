84b37f4ecdef1828014634e210472d986afb7270
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-05-13 17:00:08 +0200

    Add tests

---
+++ recipes/recipes_emscripten/protobuf/test_import_protobuf.py
@@ -0,0 +1,4 @@
+def test_protobuf():
+    from google.protobuf.descriptor_pb2 import FieldDescriptorProto
+    from google.protobuf.internal import api_implementation
+    from google.protobuf.proto_builder import MakeSimpleProtoClass
